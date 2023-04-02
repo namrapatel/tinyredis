7191bee4a41fb3a827be1a76763aef27f7de31f3
Namra Patel <namrajpatel@gmail.com>
2023-04-02 19:47:58 -0400

    impl server from main

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,7 @@
+mod server;
+
 use std::{io::{Read,Write,Result}};
 use tokio::{net::{TcpListener, TcpStream}, io::{AsyncWriteExt, AsyncReadExt}};
 use std::str;
+use server:Server;
 
-const MESSAGE_SIZE: usize = 512;
-
-async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
-    loop { 
-        let mut buffer = [0; MESSAGE_SIZE];
-        _ = stream.read(&mut buffer).await?;
-        
-        let message =  str::from_utf8(&buffer).unwrap();
-        // println!("Message recieved: {}", {message});
-        match message {
-            "*1\n$4\nPING\n" => {
-                let string = "+PONG\r\n";
-                let reply = str::as_bytes(&string);
-                _ = stream.write_all(&reply);
-            },
-            _ => { break }
-        }   
-    }
-    Ok(())
-}
-#[tokio::main]
-async fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
-
-    loop {
-        let incoming = listener.accept().await;
-
-        match incoming {
-            Ok((mut stream, addr)) => {
-                tokio::spawn(async move {
-                    handle_connection(&mut stream).await.unwrap();
-                });
-            },
-            Err(e) => {
-                println!("Error: {e}");
-            }
-        }
-    }
-}
---
+++ src/server.rs
@@ -0,0 +1,51 @@
+use std::{io::{Read,Write,Result}};
+use tokio::{net::{TcpListener, TcpStream}, io::{AsyncWriteExt, AsyncReadExt}};
+use std::str;
+
+const MESSAGE_SIZE: usize = 512;
+
+pub struct Server {
+    listener: TcpListener
+}
+
+impl Server {
+
+    async fn run() -> Result<()> {
+        let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+        loop {
+            let incoming = listener.accept().await;
+
+            match incoming {
+                Ok((mut stream, addr)) => {
+                    tokio::spawn(async move {
+                        Self::handle_connection(&mut stream).await.unwrap();
+                    });
+                },
+                Err(e) => {
+                    println!("Error: {e}");
+                }
+            }
+        }
+    }
+
+    async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+        loop { 
+            let mut buffer = [0; MESSAGE_SIZE];
+            _ = stream.read(&mut buffer).await?;
+            
+            let message =  str::from_utf8(&buffer).unwrap();
+            // println!("Message recieved: {}", {message});
+            match message {
+                "*1\n$4\nPING\n" => {
+                    let string = "+PONG\r\n";
+                    let reply = str::as_bytes(&string);
+                    _ = stream.write_all(&reply);
+                },
+                _ => { break }
+            }   
+        }
+        Ok(())
+    }
+
+}
