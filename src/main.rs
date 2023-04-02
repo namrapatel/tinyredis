95c5d111c182fd2fff72a20be72f6d9fcce378e2
Namra Patel <namrajpatel@gmail.com>
2023-04-02 13:53:59 -0400

    add my old resp

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use std::net::{TcpListener, TcpStream};
+use std::{net::{TcpListener, TcpStream}, io::{Read,Write}};
 
-fn handle_client(stream: TcpStream) {
+const MESSAGE_SIZE: usize = 1024;
 
+fn handle_connection(mut stream: TcpStream) {
+    loop {
+        println!("hello");
+        let mut buffer = [0; MESSAGE_SIZE];
+        let read_data = stream.read(&mut buffer);
+
+        println!("Received: {:?}", buffer);
+
+        stream.write_all(b"Hello from Rust!\n").unwrap();
+    }
 }
 
 fn main() -> std::io::Result<()> {
+    println!("here");
     let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
-        handle_client(stream?);
+        handle_connection(stream?);
     }
 
     Ok(())
