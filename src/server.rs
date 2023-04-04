56e49a1551443aafb77b7b3ca169355d88e3e61e
Namra Patel <namrajpatel@gmail.com>
2023-04-03 20:00:41 -0400

    get and set work, tests pass

--- src/server.rs
+++ src/server.rs
@@ -1,32 +1,34 @@
 use anyhow::{Result, Error};
 use crate::{resp::RESPMessage, cache::{Cache, self}};
 use tokio::{net::{TcpListener, TcpStream}, io::{AsyncWriteExt, AsyncReadExt}};
+use std::sync::{Arc, Mutex};
 
 const MESSAGE_SIZE: usize = 512;
 
 pub struct Server {
     listener: TcpListener,
-    cache: Cache
+    cache: Arc<Mutex<Cache>>
 }
 
 impl Server {
 
     pub async fn new() -> Result<Self, Error> {
         let listener = TcpListener::bind("127.0.0.1:6379").await?;
-        let mut cache = Cache::new();
+        let cache = Arc::new(Mutex::new(Cache::new()));
     
         Ok(Self { listener, cache })
     }
     
     pub async fn run(server: Server) -> Result<()> {
-
         loop {
             let incoming = server.listener.accept().await;
+
             match incoming {
                 Ok((mut stream, addr)) => {
                     println!("Handling connection from: {}", addr);
+                    let cache = Arc::clone(&server.cache);
                     tokio::spawn(async move {
-                        Self::handle_connection(&mut stream, &mut server.cache).await.unwrap();
+                        Self::handle_connection(&mut stream, cache).await.unwrap();
                     });
                 },
                 Err(e) => {
@@ -36,7 +38,7 @@ impl Server {
         }
     }
 
-    async fn handle_connection(stream: &mut TcpStream, cache: &mut Cache) -> Result<()> {
+    async fn handle_connection(stream: &mut TcpStream, cache: Arc<Mutex<Cache>>) -> Result<()> {
         let mut buffer = [0; MESSAGE_SIZE];
 
         loop { 
@@ -45,21 +47,54 @@ impl Server {
                 println!("Closing connection.");
                 break;
             }
-            // println!("Buffer contents: {:?}", String::from_utf8_lossy(&buffer));
 
             let (message, _) = RESPMessage::deserialize(&buffer);
-            // println!("Message recieved: {:?}", {message.clone()});
 
             let (command, args) = message.to_command()?;
-            // println!("Here is the command: {}", {command.clone()});
             let response = match command.to_ascii_lowercase().as_ref() {
                 "ping" => {
                     RESPMessage::SimpleString("PONG".to_string())
                 },
                 "echo" => args.first().unwrap().clone(),
+                "get" => {
+                    let key = args.get(0).map(|arg| arg.pack_string());
+    
+                    match key {
+                        Some(Ok(key)) => match cache.lock().unwrap().get(key.as_ref()) {
+                            Some(value) => RESPMessage::BulkString(value),
+                            None => RESPMessage::Null,
+                        },
+                        _ => RESPMessage::Error("Invalid key".to_string()),
+                    }
+                },
                 "set" => {
-
-                }
+                    let key = args.get(0).map(|arg| arg.pack_string());
+                    let value = args.get(1).map(|arg| arg.pack_string());
+                    let px = args.get(3).map(|arg| arg.pack_string());
+                
+                    match (key, value) {
+                        (Some(Ok(key)), Some(Ok(value))) => {
+                            let result: Result<(), ()> = match px {
+                                Some(Ok(px)) => {
+                                    let ttl = px.parse::<u64>().ok().map(|ms| ms / 1000);
+                                    let mut cache = cache.lock().unwrap();
+                                    cache.set(key.to_string(), value.to_string(), ttl);
+                                    Ok(())
+                                },
+                                _ => {
+                                    let mut cache = cache.lock().unwrap();
+                                    cache.set(key.to_string(), value.to_string(), None);
+                                    Ok(())
+                                },
+                            };
+                            match result {
+                                Ok(_) => RESPMessage::SimpleString("OK".to_string()),
+                                Err(_) => RESPMessage::Error("Error".to_string()),
+                            }
+                        },
+                        _ => RESPMessage::Error("Invalid key or value".to_string()),
+                    }
+                },                                                       
                 _ => RESPMessage::Error("Error".to_string())
             };
             let serialized_response = RESPMessage::serialize(&response);
@@ -67,5 +102,4 @@ impl Server {
         }
         Ok(())
     }
-
 }
